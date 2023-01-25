570ce3bd15818d48b70f6b693142399e031a966e
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-01-25 17:20:35 -0600

    build reverseString function and enable tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    let stringArray = string.split("");
+    let revString = "";
 
+    for ( let i = (string.length - 1); i >= 0; i--) {
+        revString = revString + stringArray[i];
+    }
+
+    return revString;
 };
 
 // Do not edit below this line

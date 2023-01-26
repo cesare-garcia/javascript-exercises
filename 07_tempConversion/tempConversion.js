ed13d17a3d9705aa3834e71ad7eba632e75f869f
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-01-26 17:17:20 -0600

    build tempConversion functions and enable tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp_f) {
+  let celsius_temp = (temp_f - 32) * (5/9);
+  return Number(celsius_temp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp_c) {
+  let fahrenheit_temp = (temp_c * 1.8) + 32;
+  return Number(fahrenheit_temp.toFixed(1));
 };
 
 // Do not edit below this line

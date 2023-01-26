d4aeb4ef4568c0c15ed9bb33a8ad9af4f497e8f4
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-01-26 15:02:45 -0600

    build sumAll function and enable first four tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function( starting_int, ending_int ) {
 
+    let larger_int = 0;
+    let smaller_int = 0;
+    let sum = 0;
+
+    if ( starting_int < 0 || ending_int < 0 ) {
+        return "ERROR";
+    }
+
+    if ( ending_int > starting_int ) {
+        larger_int = ending_int;
+        smaller_int = starting_int;
+    } else {
+        larger_int = starting_int;
+        smaller_int = ending_int;
+    }
+
+    for ( let i = smaller_int; i <= larger_int; i++ ) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

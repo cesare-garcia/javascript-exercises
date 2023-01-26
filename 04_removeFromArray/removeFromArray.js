f6921663d97d848d21e4f0c610d40088a6fbf566
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-01-26 14:19:49 -0600

    modify removeFromArray function to pass all 7 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,16 @@
-const removeFromArray = function( array, targetElement1, targetElement2, targetElement3, targetElement4 ) {
-    
+
+
+const removeFromArray = function(array, ...args) {
+
     let newArray = [];
-    
-    for ( const element of array ) {
-        if ( element != targetElement1 && element != targetElement2 && element != targetElement3 && element != targetElement4 ) {
-            newArray.push(element);
-        } else {
-            continue;
+        
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
         }
-    }
-
-    return newArray
-
+    });
+    
+    return newArray;
 };
 
 // Do not edit below this line

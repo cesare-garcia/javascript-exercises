190445a08af4d06340c4727b6e328e7f8c18ab6e
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-01-25 18:04:20 -0600

    build removeFromArray Function and enable 6 tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function( array, targetElement1, targetElement2, targetElement3, targetElement4 ) {
+    
+    let newArray = [];
+    
+    for ( const element of array ) {
+        if ( element != targetElement1 && element != targetElement2 && element != targetElement3 && element != targetElement4 ) {
+            newArray.push(element);
+        } else {
+            continue;
+        }
+    }
+
+    return newArray
 
 };
 

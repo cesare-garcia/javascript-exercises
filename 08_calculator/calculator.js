e8ec832d9fb0ca5fda3da1bdfcb34f7d30b346a2
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-01-30 21:15:30 -0600

    Complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  let sum = num1 + num2;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	let difference = num1 - num2;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let arraySum = 0;
+  for ( let i = 0; i < array.length; i++ ) {
+    arraySum += array[i];
+  }
+  return arraySum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for ( let i = 0; i < array.length; i++ ) {
+    product *= array[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	let product = base ** exponent;
+  return product;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	let factorialArray = [];
+  let factorialProduct = 1;
+
+  if ( number == 0 ) {
+    let factorialProduct = 1;
+    return factorialProduct;
+
+  } else {
+    
+    for (let i = 1; i <= number; i++) {
+      factorialArray.push(i);
+      factorialProduct *= factorialArray[i-1];
+    }
+
+    return factorialProduct;
+  }
 };
 
 // Do not edit below this line

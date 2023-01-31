dfc622dae7bd6cfb2ebb5313b26f2cbc30c1c773
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-01-30 22:21:01 -0600

    complete exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(newArray) {
 
+   let titleArray = [];
+
+   newArray.forEach(element => {
+        titleArray.push(element.title);
+   })
+
+   return titleArray;
 };
 
 // Do not edit below this line

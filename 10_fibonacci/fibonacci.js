e1c0589a4e9dda7504e4dd00d122aa0260648158
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-01-30 22:12:27 -0600

    complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(sequenceNumber) {
+
+    let cSequenceNumber = parseInt(sequenceNumber);
+
+    let fibArray = [1,1];
+
+    if ( cSequenceNumber < 0 ) {
+        return "OOPS";
+    } else {
+        for ( let i = 0; i < cSequenceNumber; i++ ) {
+            fibArray.push((fibArray[i]+fibArray[i+1]))
+        }
+        return fibArray[cSequenceNumber-1];
+    }
 
 };
 

1bf4b52b956049a4c3a476e23a289d13832eb6e1
Cesar Garcia <79774422+cesare-garcia@users.noreply.github.com>
2023-02-03 15:17:04 -0600

    complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,50 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    let originalString = string.toUpperCase();
+    let testArray = originalString.split("");
+    let originalStringCheck = '';
+    for ( let i = 0; i < testArray.length; i++ ) {
+        if ( testArray[i] == '.' || testArray[i] == ',' || testArray[i] == '!' || testArray[i] == '?' || testArray[i] == ' ' ) {
+            continue;
+        } else {
+            originalStringCheck += testArray[i];
+        }
+    }
+    if ( testArray[testArray.length - 1] == "." || testArray[testArray.length - 1] == "," || testArray[testArray.length - 1] == "!" || testArray[testArray.length - 1] == "?") {
+        originalStringCheck += testArray[testArray.length - 1];
+    }
+    
+    let reverseString = '';
+    
+    if ( testArray[testArray.length - 1] == '.' || testArray[testArray.length - 1] == ',' || testArray[testArray.length - 1] == '!' || testArray[testArray.length - 1] == '?' ) {
+        for ( let i = (testArray.length - 2); i >= 0; i-- ) {
+            if ( testArray[i] == '.' || testArray[i] == ',' || testArray[i] == '!' || testArray[i] == '?' || testArray[i] == ' ' ) {
+                continue;
+            } else {
+                reverseString += testArray[i];
+            }
+        }
+        reverseString += testArray[testArray.length - 1];
+    } else {
+        for (let i = (testArray.length - 1); i >= 0; i-- ) {
+            if ( testArray[i] == '.' || testArray[i] == ',' || testArray[i] == '!' || testArray[i] == '?' || testArray[i] == ' ' ) {
+                continue;
+            } else {
+                reverseString += testArray[i];
+            }
+        }
+    }
+    console.log(originalStringCheck);
+    console.log(reverseString);
+    if (originalStringCheck == reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
